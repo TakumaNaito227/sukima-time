0cfccd0aa742e2f00ea452c33e768effcb161c4f
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2020-06-06 15:19:19 +0900

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :cities
 
-  belongs_to :users
+  belongs_to :user
 
   validates :nickname, :title, :year, :month, :day, :start, :end, :money, :text, :user_id, :city_id, presence: true
 end
